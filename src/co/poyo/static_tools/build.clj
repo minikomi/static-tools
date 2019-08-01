a27dae5bb2be3db10362e954132747aae07869a3
Adam Moore <nerdfunk@gmail.com>
2019-08-01 18:15:34 +0900

    use opts for build too

--- src/co/poyo/static_tools/build.clj
+++ src/co/poyo/static_tools/build.clj
@@ -1,20 +1,39 @@
 (ns co.poyo.static-tools.build
   (:require
    [mount.core :as mount]
+   [co.poyo.static-tools.opt :as opt]
    [co.poyo.static-tools.pages :as pages]
    [co.poyo.static-tools.garden :as garden]
    [co.poyo.static-tools.config :as config]
+   [co.poyo.static-tools.target-dir  :as t]
    [co.poyo.watch-and-run.file-map :as file-map]
+   [taoensso.timbre :as timbre]
    [me.raynes.fs :as fs]
    [figwheel.main.api :as figwheel]
    [clojure.java.io :as io]))
 
 (defn -main [& args]
-  (mount/start (mount/only #{#'config/env}))
-  (when (fs/exists? (:target config/env)) (fs/delete-dir (:target config/env)))
-  (doseq [job (garden/get-css-jobs)] ((:build-fn job)))
-  (doseq [job (pages/get-pages-jobs)] ((:build-fn job)))
-  (doseq [build (get config/env :cljs-builds [:prod])]
-    (figwheel/start {:mode :build-once :open-url false} build))
-  (when (fs/exists? (io/file (:target config/env) "cljs-out"))
-    (fs/delete-dir (io/file (:target config/env) "cljs-out"))))
+  (mount/start (mount/only #{#'config/env
+                             #'t/target-dir}))
+  (let [{:keys [css js pages static] :as opts} (opt/parse args)]
+    (println opts)
+    (when css
+      (doseq [{:keys [build-fn]}
+              (garden/get-css-jobs)]
+        (build-fn)))
+    (when pages
+      (doseq [{:keys [build-fn]}
+              (pages/get-pages-jobs)]
+        (build-fn)))
+    (when js
+      (doseq [build (get config/env :cljs-builds [:prod])]
+        (figwheel/start {:mode :build-once :open-url false} build))
+      (when (fs/exists? (io/file (:target config/env) "cljs-out"))
+        (fs/delete-dir (io/file (:target config/env) "cljs-out"))))
+    (when static
+      (doseq [f (fs/list-dir (get config/env :static "./static"))
+              :when (not (#{".DS_Store"} (.getName f)))]
+        (timbre/infof "copying [%s]\n" (.getName f))
+        (if (fs/directory? f)
+          (fs/copy-dir f (io/file t/target-dir (.getName f)))
+          (fs/copy f (io/file t/target-dir (.getName f))))))))
--- src/co/poyo/static_tools/opt.clj
+++ src/co/poyo/static_tools/opt.clj
@@ -5,6 +5,7 @@
 (def cli-options
   [["-c" "--css"   "build css"]
    ["-j" "--js"    "build js"]
+   ["-s" "--static" "copy static to target"]
    ["-p" "--pages" "build html pages"]])
 
 (defn parse [args]
