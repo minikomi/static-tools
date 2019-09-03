91a0dc870fae199e4bf4f15c85e8a9080bc176a6
Adam Moore <nerdfunk@gmail.com>
2019-09-03 16:34:55 +0900

    keep not-mounted in atom to allow restart

--- src/co/poyo/static_tools/dev.clj
+++ src/co/poyo/static_tools/dev.clj
@@ -6,8 +6,9 @@
    [co.poyo.static-tools.config      :as config]
    [co.poyo.static-tools.nrepl       :as n]
    [co.poyo.static-tools.target-dir  :as t]
-   [co.poyo.static-tools.http-server :as h]
    ;; switchable
+   [co.poyo.static-tools.http-server :as server]
+   [co.poyo.static-tools.handler :as handler]
    [co.poyo.static-tools.pages    :as p]
    [co.poyo.static-tools.garden   :as g]
    [co.poyo.static-tools.figwheel :as f]
@@ -18,9 +19,11 @@
    [taoensso.timbre :as timbre]
    [me.raynes.fs :as fs]))
 
+(def not-mounted (atom #{}))
+
 (defn start! []
   (->
-   (mount/except [#'n/nrepl])
+   (mount/except (conj @not-mounted #'n/nrepl))
    (mount/start)))
 
 (defn stop! []
@@ -33,14 +36,16 @@
   (start!))
 
 (defn -main [& args]
-  (let [{:keys [css js pages] :as opts} (opt/parse args)
-        not-mounted (cond-> #{}
-                      (not css)   (conj #'g/garden)
-                      (not js)    (conj #'f/figwheel)
-                      (not pages) (conj #'p/simple-pages))]
+  (let [{:keys [css js pages server] :as opts} (opt/parse args)]
     (println "OPTS:" opts)
+    (reset! not-mounted
+            (cond-> #{}
+              (not css)    (conj #'g/garden)
+              (not server) (conj #'server/server #'handler/wrapped-handler)
+              (not js)     (conj #'f/figwheel)
+              (not pages)  (conj #'p/simple-pages)))
     (timbre/info
      (with-out-str
        (pprint/pprint
-        (-> (mount/except not-mounted)
+        (-> (mount/except @not-mounted)
             (mount/start)))))))
